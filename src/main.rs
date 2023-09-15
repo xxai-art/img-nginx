550eef55f690688dacf692121d9e27a020822d95
xxai.art <xxai.art@gmail.com>
2023-09-15 11:01:37 +0800

    ♨

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,22 @@ use serde::Deserialize;
 #[derive(Debug, PartialEq, Deserialize)]
 struct 配置 {
   端口: u16,
+  写: String,
   参: String,
   静: HashMap<String, u32>,
   动: HashMap<String, u32>,
 }
 
 fn main() -> Result<()> {
-  let mut 配置文件 = env::current_dir()?;
-  配置文件.push("conf.yml");
-  let conf = read_to_string(配置文件);
-  dbg!(conf);
+  let pwd = env::current_dir()?;
+  let conf: 配置 = serde_yaml::from_str(&read_to_string(pwd.join("conf.yml"))?)?;
+
+  let 写 = if !conf.写.starts_with('/') {
+    pwd.join(conf.写)
+  } else {
+    conf.写.into()
+  };
+
+  dbg!(写);
   Ok(())
 }
