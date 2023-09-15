e822660b10a1d53ff1988b3c860f83c6b5ee8def
xxai.art <xxai.art@gmail.com>
2023-09-15 10:53:38 +0800

    ♨

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::{collections::HashMap, env};
+
+use anyhow::Result;
+use serde::Deserialize;
+
+#[derive(Debug, PartialEq, Deserialize)]
+struct Point {
+  端口: u16,
+  参: String,
+  静: HashMap<String, u32>,
+  动: HashMap<String, u32>,
+}
+
+fn main() -> Result<()> {
+  let mut conf = env::current_dir()?;
+  conf.push("conf.yml");
+  println!("Hello, world! {:?}", conf);
+  Ok(())
 }
